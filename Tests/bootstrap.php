646dd03cba4350cc9608bd3d3b54137d7cd72eb1
Alvaro <alvaro.garcia@roams.es>
2017-02-16 17:35:49 +0100

    travis

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,18 @@
 <?php
-
-$file = __DIR__.'/../vendor/autoload.php';
-if (!file_exists($file)) {
-    throw new RuntimeException('Install dependencies to run test suite.');
+$vendorDir = __DIR__ . '/../vendor';
+if (!@include($vendorDir . '/.composer/autoload.php')) {
+    die("You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+");
 }
-$autoload = require_once $file;
+spl_autoload_register(function($class) {
+    if (0 === (strpos($class, 'Wixet\\RecaptchaBundle\\'))) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 3)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+        return true;
+    }
+});
